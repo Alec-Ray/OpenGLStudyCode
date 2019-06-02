a8d322b0fd87281ff34ba57b105122b720a6db43
WJGwjg <1056929926@qq.com>
2019-06-02 11:10:20 +0800

    CoordSystem

---
+++ WjgOGL/OGL_06_CoordSystemTenCube/main_06_CSTC.cpp
@@ -0,0 +1,276 @@
+#include "includes\glad\glad.h"
+#include "includes\glfw\glfw3.h"
+#include <iostream>
+#include "includes\Shader.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "includes\stb_image.h"
+
+#include "includes\glm\glm.hpp"
+#include "includes\glm\gtc\matrix_transform.hpp"
+#include "includes\glm\gtc\type_ptr.hpp"
+
+const unsigned int Screen_Width  = 1000;
+const unsigned int Screen_Height = 800;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef  _APPLE_
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif //  _APPLE_
+
+	GLFWwindow* window = glfwCreateWindow(Screen_Width, Screen_Height, "WjgOGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "failed to create glfw window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "failed to initilize glad" << std::endl;
+		return -1;
+	}
+
+	//开启深度缓冲。开启后会一直开启，除非你手动再关闭
+	glEnable(GL_DEPTH_TEST);
+	
+	Shader MyShader("includes/shader.vs", "includes/shader.fs");
+
+	//立方体，36个顶点
+	float vertices[] = {
+		//   -- 位置 --     -- 纹理坐标 --
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+		0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+	};
+
+	//本节不需要用到EBO(索引缓冲对象)，因为我们直接用了36个顶点数据来构建立方体
+	//如果用EBO的话，应该8个顶点就够了
+	////索引
+	//unsigned int indices[] = { // 注意索引从0开始! 
+	//	0, 1, 3, // 第一个三角形
+	//	1, 2, 3  // 第二个三角形
+	//};
+
+	//世界坐标系下10个立方体的位置
+	glm::vec3 cubePositions[] = {
+		glm::vec3( 0.0f,  0.0f,  0.0f),
+		glm::vec3( 2.0f,  5.0f, -15.0f),
+		glm::vec3(-1.5f, -2.2f, -2.5f),
+		glm::vec3(-3.8f, -2.0f, -12.3f),
+		glm::vec3( 2.4f, -0.4f, -3.5f),
+		glm::vec3(-1.7f,  3.0f, -7.5f),
+		glm::vec3( 1.3f, -2.0f, -2.5f),
+		glm::vec3( 1.5f,  2.0f, -2.5f),
+		glm::vec3( 1.5f,  0.2f, -1.5f),
+		glm::vec3(-1.3f,  1.0f, -1.5f)
+	};
+	
+	//顶点缓冲对象
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	//顶点数组对象
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+
+	//顶点索引对象
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	//设置顶点(位置)属性指针
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	//设置纹理坐标属性指针
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+
+	//翻转图片的y轴
+	stbi_set_flip_vertically_on_load(true);
+
+	//生成第一个纹理
+	unsigned int texture01;
+	glGenTextures(1, &texture01);
+	glBindTexture(GL_TEXTURE_2D, texture01);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+
+	int width, height, nrChannels;
+	unsigned char *data = stbi_load("textures/container.jpg", &width, &height, &nrChannels, 0);
+
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "failed to load texture" << std::endl;
+	}
+
+	stbi_image_free(data);
+
+	//生成第二个纹理
+	unsigned int texture02;
+	glGenTextures(1, &texture02);
+	glBindTexture(GL_TEXTURE_2D, texture02);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+
+	data = stbi_load("textures/face.png", &width, &height, &nrChannels, 0);
+
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "failed to load texture" << std::endl;
+	}
+
+	stbi_image_free(data);
+
+
+	//解除绑定，注意解绑顺序
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);	
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+	MyShader.use();
+	glUniform1i(glGetUniformLocation(MyShader.ID, "texture01"), 0);
+	MyShader.setInt("texture02", 1);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		processInput(window);
+
+		glClearColor(0.3f, 0.6f, 0.5f ,1.0f);
+		//加上清除深度缓冲buffer
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture01);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, texture02);
+
+		glm::mat4 view = glm::mat4(1.0f);				//观察者矩阵
+		glm::mat4 projection = glm::mat4(1.0f);			//透视投影矩阵
+
+		//我们将矩阵向我们要进行移动场景的反方向移动
+		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -4.0f));
+		projection = glm::perspective(glm::radians(45.0f),(float) Screen_Width / Screen_Height, 0.1f, 100.0f);
+
+		MyShader.use();
+
+		int viewLoc = glGetUniformLocation(MyShader.ID, "view");
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
+		MyShader.setMat4("projection", projection);
+
+		glBindVertexArray(VAO);
+
+		//绘制10个在旋转的立方体
+		for (int i = 0; i < 10; i++)
+		{
+			glm::mat4 model = glm::mat4(1.0f);				//模型矩阵，局部坐标到世界坐标
+			model = glm::translate(model, cubePositions[i]);
+			float angle = 15.0f * (i+1);
+			model = glm::rotate(model, (float)glfwGetTime() * glm::radians(angle), glm::vec3(1.0f, 0.4f, 0.6f));
+			MyShader.setMat4("model", model);
+
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}				
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	}
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
+	glDeleteTextures(1, &texture01);
+	glDeleteTextures(1, &texture02);
+
+	glfwTerminate();
+	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow * window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow * window)
+{
+	if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
+		//按空格键进入线框模式(Wireframe Mode)
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	}		
+
+}
