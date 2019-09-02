a470db6dcf830b9f0946333d20f8ca9ef5d2c165
unknown <wujiangang@ztgame.com>
2019-09-02 18:01:21 +0800

    StencilTest

---
+++ WjgOGL/OGL_16_StencilTest/main_16.cpp
@@ -0,0 +1,370 @@
+#include "includes\glad\glad.h"
+#include "includes\glfw\glfw3.h"
+#include <iostream>
+
+#include "includes\Shader.h"
+#include "includes\camera.h"
+#include "includes\model.h"
+
+#include "includes\glm\glm.hpp"
+#include "includes\glm\gtc\matrix_transform.hpp"
+#include "includes\glm\gtc\type_ptr.hpp"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "includes\stb_image.h"
+
+#pragma comment ( lib,"glfw3.lib" ) 
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+unsigned int loadTexture(const char *path);
+
+const unsigned int Screen_Width  = 1000;
+const unsigned int Screen_Height = 800;
+
+float lastX = Screen_Width/2.0f;
+float lastY = Screen_Height/2.0f;
+
+float deltaTime = 0.0f;		
+float lastFrame = 0.0f;	
+
+Camera camera(glm::vec3(0.0f, 0.0f, 5.0f));
+bool firstMouse = true;
+
+glm::vec3 lightPos(1.0f, 1.0f, 2.0f);
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef  _APPLE_
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif //  _APPLE_
+
+	GLFWwindow* window = glfwCreateWindow(Screen_Width, Screen_Height, "WjgOGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "failed to create glfw window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "failed to initilize glad" << std::endl;
+		return -1;
+	}
+
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LESS);
+
+	//glStencilOp(GLenum sfail, GLenum dpfail, GLenum dppass)
+	//sfail：模板测试失败时采取的行为。
+	//dpfail：模板测试通过，但深度测试失败时采取的行为。
+	//dppass：模板测试和深度测试都通过时采取的行为。
+
+	//启用模板测试
+	glEnable(GL_STENCIL_TEST);
+	//符合GL_NOTEQUAL的通过模板测试，参考值为1(ref)
+	glStencilFunc(GL_NOTEQUAL, 1, 0xFF);
+	//GL_KEEP	保持当前储存的模板值
+	//GL_REPLACE	将模板值设置为glStencilFunc函数设置的ref值
+	glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);
+
+	
+	Shader MyShader("includes/shader.vs", "includes/shader.fs");
+	Shader OutlineShader("includes/shader.vs", "includes/OutlineShader.fs");
+	
+	float cubeVertices[] = {
+		// 顶点数据          // 纹理坐标
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+		0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+	};
+	//地面
+	float planeVertices[] = {	
+		//地面纹理坐标大于1，图像在表面上重复出现，GL_REPEAT
+		5.0f, -0.5f,  5.0f,  2.0f, 0.0f,
+		-5.0f, -0.5f,  5.0f,  0.0f, 0.0f,
+		-5.0f, -0.5f, -5.0f,  0.0f, 2.0f,
+
+		5.0f, -0.5f,  5.0f,  2.0f, 0.0f,
+		-5.0f, -0.5f, -5.0f,  0.0f, 2.0f,
+		5.0f, -0.5f, -5.0f,  2.0f, 2.0f								
+	};
+
+	//箱子VAO
+	unsigned int cubeVAO, cubeVBO;
+	glGenVertexArrays(1, &cubeVAO);
+	glGenBuffers(1, &cubeVBO);
+	glBindVertexArray(cubeVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), &cubeVertices, GL_STATIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	glBindVertexArray(0);
+	// 地面VAO
+	unsigned int planeVAO, planeVBO;
+	glGenVertexArrays(1, &planeVAO);
+	glGenBuffers(1, &planeVBO);
+	glBindVertexArray(planeVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, planeVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(planeVertices), &planeVertices, GL_STATIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	glBindVertexArray(0);
+	
+	unsigned int cubeTexture = loadTexture("textures/container2.png");
+	unsigned int floorTexture = loadTexture("textures/metal.png");
+
+	MyShader.use();
+	MyShader.setInt("texture1", 0);
+
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+	/*
+	原理概述：
+		代码上其实就是画了两个图形。一般情况下，两个图形都会画出来（当然深度测试会挡住一部分）。
+		现在加入了模板测试，那么第一个画的图形的模板值全替换为1(关键操作GL_REPLACE 和 开启缓冲输入）。
+		第二个图形，放大一点点，换个单色的着色器，那么一般情况下，会有有一个大一圈的单色模型挡住了第一个图形（深度测试的锅）。
+		哪怕关了深度测试，图形依然是一片绿的（后绘制的覆盖前绘制的）。
+		现在利用第一个图形那部分模板值全是1的特点，运用函数设置为当模板值不等于1时，第二个图形进行绘制。那么效果就是，
+		第一个图形正常绘制，第二个图形仅有比第一个图形大的那部分绘制出来，所以看上去就是多了一个边缘。
+	*/
+
+	while (!glfwWindowShouldClose(window))
+	{
+		float currentTime = glfwGetTime();
+		deltaTime = currentTime - lastFrame;
+		lastFrame = currentTime;
+
+		processInput(window);
+
+		glClearColor(0.1, 0.1f, 0.1f ,1.0f);
+		//清除模板缓冲
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+
+		MyShader.use();
+		glm::mat4 model = glm::mat4(1.0f);
+		glm::mat4 view = camera.GetViewMatrix();
+		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)Screen_Width / (float)Screen_Height, 0.1f, 100.0f);
+		MyShader.setMat4("view", view);
+		MyShader.setMat4("projection", projection);
+
+		//描述外轮廓的的顶点着色器,可以继续使用之前的观察者矩阵和投影矩阵，模型矩阵的缩放系数变大就行了
+		OutlineShader.use();
+		OutlineShader.setMat4("view", view);
+		OutlineShader.setMat4("projection", projection);
+
+		MyShader.use();
+
+		//先绘制地面，并且关闭模板缓冲的写入
+		glStencilMask(0x00);
+		//地面
+		glBindVertexArray(planeVAO);
+		glBindTexture(GL_TEXTURE_2D, floorTexture);
+		MyShader.setMat4("model", glm::mat4(1.0f));
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+		glBindVertexArray(0);
+
+		//开始绘制两个正常的箱子，开启模板缓冲的写入，GL_ALWAYS意思为总是通过模板测试
+		glStencilFunc(GL_ALWAYS, 1, 0xFF);
+		glStencilMask(0xFF);
+		//箱子
+		glBindVertexArray(cubeVAO);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, cubeTexture); 	
+		model = glm::translate(model, glm::vec3(-1.0f, 0.0f, -1.0f));
+		MyShader.setMat4("model", model);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(2.0f, 0.0f, 0.0f));
+		MyShader.setMat4("model", model);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+
+		//绘制两个放大的箱子，关闭模板缓冲的写入
+		//GL_NOTEQUAL意思为不等于才通过模板测试,我们在绘制两个正常的箱子的时候已经把他们的片段的模板缓冲设为了1
+		//关闭深度测试，是为了不影响轮廓显示
+		glStencilFunc(GL_NOTEQUAL, 1, 0xFF);
+		glStencilMask(0x00);
+		glDisable(GL_DEPTH_TEST);
+		OutlineShader.use();
+		//两个放大的箱子，只有外轮廓通过了模板测试
+		float scale = 1.05;
+		glBindVertexArray(cubeVAO);
+		//不需要用到纹理
+		//glActiveTexture(GL_TEXTURE0);
+		//glBindTexture(GL_TEXTURE_2D, cubeTexture);
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(-1.0f, 0.0f, -1.0f));
+		//放大
+		model = glm::scale(model, glm::vec3(scale, scale, scale));
+		OutlineShader.setMat4("model", model);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(2.0f, 0.0f, 0.0f));
+		//放大
+		model = glm::scale(model, glm::vec3(scale, scale, scale));
+		OutlineShader.setMat4("model", model);
+		glDrawArrays(GL_TRIANGLES, 0, 36);		
+		glBindVertexArray(0);
+
+		//再次开启换模板缓冲写入
+		glStencilMask(0xFF);
+		//再次开启深度测试
+		glEnable(GL_DEPTH_TEST);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	}
+
+	glDeleteVertexArrays(1, &cubeVAO);
+	glDeleteVertexArrays(1, &planeVAO);
+	glDeleteBuffers(1, &cubeVBO);
+	glDeleteBuffers(1, &planeVBO);
+
+	glfwTerminate();
+	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow * window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow * window)
+{
+	if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	}
+	if (glfwGetKey(window, GLFW_KEY_ENTER) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
+
+	float cameraSpeed = 2.5f * deltaTime;
+
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.ProcessKeyboard(FORWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.ProcessKeyboard(BACKWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.ProcessKeyboard(LEFT, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.ProcessKeyboard(RIGHT, deltaTime);
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	std::cout << xpos << "+" << ypos << std::endl;
+	
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+	
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos;
+
+	lastX = xpos;
+	lastY = ypos;
+
+	camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	camera.ProcessMouseScroll(yoffset);
+}
+
+unsigned int loadTexture(const char * path)
+{
+	unsigned int textureID;
+	glGenTextures(1, &textureID);
+
+	int width, height, nrComponents;
+	unsigned char *data = stbi_load(path, &width, &height, &nrComponents, 0);
+	if (data)
+	{
+		GLenum format;
+		if (nrComponents == 1)
+			format = GL_RED;
+		else if (nrComponents == 3)
+			format = GL_RGB;
+		else if (nrComponents == 4)
+			format = GL_RGBA;
+
+		glBindTexture(GL_TEXTURE_2D, textureID);
+		glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		stbi_image_free(data);
+	}
+	else
+	{
+		std::cout << "Texture failed to load at path: " << path << std::endl;
+		stbi_image_free(data);
+	}
+
+	return textureID;
+}
