e1c901a89e9f097d9dbf92799fb5998e5547bd5f
WJGwjg <1056929926@qq.com>
2019-11-03 19:47:45 +0800

    GeometryShaderDemoAndFourHouse

---
+++ WjgOGL/OGL_22_GeometryShaderDemo1/includes/Shader.h
@@ -0,0 +1,278 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#include "glad\glad.h"
+#include "glm\glm.hpp"
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+class Shader
+{
+public:
+	//程序ID
+	unsigned int ID;
+
+	//构建着色器
+	Shader(const GLchar* vertexPath,const GLchar* fragmentPath);
+	Shader(const char* vertexPath, const char* fragmentPath, const char* geometryPath);
+
+	//激活着色器程序
+	void use();
+
+	//uniform工具函数
+	void setBool(const std::string &name,bool value) const;
+	void setInt(const std::string &name,int value) const;
+	void setFloat(const std::string &name,float value) const;
+	void setVec2(const std::string &name, const glm::vec2 &value) const;
+	void setVec2(const std::string &name, float x, float y) const;
+	void setVec3(const std::string &name, const glm::vec3 &value) const;
+	void setVec3(const std::string &name, float x, float y, float z) const;
+	void setVec4(const std::string &name, const glm::vec4 &value) const;
+	void setVec4(const std::string &name, float x, float y, float z, float w) const;
+	void setMat2(const std::string &name, const glm::mat2 &mat) const;
+	void setMat3(const std::string &name, const glm::mat3 &mat) const;
+	void setMat4(const std::string &name, const glm::mat4 &mat) const;
+
+private:
+	void checkCompileErrors(unsigned int shader, std::string type);
+
+};
+
+inline Shader::Shader(const GLchar* vertexPath, const GLchar* fragmentPath)
+{
+	// 1. 从文件路径中获取顶点/片段着色器
+	std::string vertexCode;
+	std::string fragmentCode;
+	std::ifstream vShaderFile;
+	std::ifstream fShaderFile;
+	// 保证ifstream对象可以抛出异常：
+	vShaderFile.exceptions (std::ifstream::failbit | std::ifstream::badbit);
+	fShaderFile.exceptions (std::ifstream::failbit | std::ifstream::badbit);
+	try 
+	{
+		// 打开文件
+		vShaderFile.open(vertexPath);
+		fShaderFile.open(fragmentPath);
+
+		// 读取文件的缓冲内容到数据流中
+		std::stringstream vShaderStream, fShaderStream;
+		vShaderStream << vShaderFile.rdbuf();
+		fShaderStream << fShaderFile.rdbuf();
+
+		// 关闭文件处理器
+		vShaderFile.close();
+		fShaderFile.close();
+
+		// 转换数据流到string
+		vertexCode   = vShaderStream.str();
+		fragmentCode = fShaderStream.str();
+	}
+	catch (std::ifstream::failure e)
+	{
+		std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
+	}
+
+	const char* vShaderCode = vertexCode.c_str();
+	const char * fShaderCode = fragmentCode.c_str();
+
+	// 2. 编译shader
+	unsigned int vertex, fragment;
+	// vertex shader
+	vertex = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertex, 1, &vShaderCode, NULL);
+	glCompileShader(vertex);
+	checkCompileErrors(vertex, "VERTEX");
+	// fragment Shader
+	fragment = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragment, 1, &fShaderCode, NULL);
+	glCompileShader(fragment);
+	checkCompileErrors(fragment, "FRAGMENT");
+	// shader Program
+	ID = glCreateProgram();
+	glAttachShader(ID, vertex);
+	glAttachShader(ID, fragment);
+	glLinkProgram(ID);
+
+	// 打印连接错误（如果有的话）
+	checkCompileErrors(ID, "PROGRAM");
+
+	// 删除着色器，它们已经链接到我们的程序中了，已经不再需要了
+	glDeleteShader(vertex);
+	glDeleteShader(fragment);
+}
+
+inline Shader::Shader(const char * vertexPath, const char * fragmentPath, const char * geometryPath)
+{
+	// 1. retrieve the vertex/fragment source code from filePath
+	std::string vertexCode;
+	std::string fragmentCode;
+	std::string geometryCode;
+	std::ifstream vShaderFile;
+	std::ifstream fShaderFile;
+	std::ifstream gShaderFile;
+	// ensure ifstream objects can throw exceptions:
+	vShaderFile.exceptions (std::ifstream::failbit | std::ifstream::badbit);
+	fShaderFile.exceptions (std::ifstream::failbit | std::ifstream::badbit);
+	gShaderFile.exceptions (std::ifstream::failbit | std::ifstream::badbit);
+	try 
+	{
+		// open files
+		vShaderFile.open(vertexPath);
+		fShaderFile.open(fragmentPath);
+		std::stringstream vShaderStream, fShaderStream;
+		// read file's buffer contents into streams
+		vShaderStream << vShaderFile.rdbuf();
+		fShaderStream << fShaderFile.rdbuf();		
+		// close file handlers
+		vShaderFile.close();
+		fShaderFile.close();
+		// convert stream into string
+		vertexCode = vShaderStream.str();
+		fragmentCode = fShaderStream.str();			
+		// if geometry shader path is present, also load a geometry shader
+		if(geometryPath != nullptr)
+		{
+			gShaderFile.open(geometryPath);
+			std::stringstream gShaderStream;
+			gShaderStream << gShaderFile.rdbuf();
+			gShaderFile.close();
+			geometryCode = gShaderStream.str();
+		}
+	}
+	catch (std::ifstream::failure e)
+	{
+		std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
+	}
+	const char* vShaderCode = vertexCode.c_str();
+	const char * fShaderCode = fragmentCode.c_str();
+	// 2. compile shaders
+	unsigned int vertex, fragment;
+	// vertex shader
+	vertex = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertex, 1, &vShaderCode, NULL);
+	glCompileShader(vertex);
+	checkCompileErrors(vertex, "VERTEX");
+	// fragment Shader
+	fragment = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragment, 1, &fShaderCode, NULL);
+	glCompileShader(fragment);
+	checkCompileErrors(fragment, "FRAGMENT");
+	// if geometry shader is given, compile geometry shader
+	unsigned int geometry;
+	if(geometryPath != nullptr)
+	{
+		const char * gShaderCode = geometryCode.c_str();
+		geometry = glCreateShader(GL_GEOMETRY_SHADER);
+		glShaderSource(geometry, 1, &gShaderCode, NULL);
+		glCompileShader(geometry);
+		checkCompileErrors(geometry, "GEOMETRY");
+	}
+	// shader Program
+	ID = glCreateProgram();
+	glAttachShader(ID, vertex);
+	glAttachShader(ID, fragment);
+	if(geometryPath != nullptr)
+		glAttachShader(ID, geometry);
+	glLinkProgram(ID);
+	checkCompileErrors(ID, "PROGRAM");
+	// delete the shaders as they're linked into our program now and no longer necessery
+	glDeleteShader(vertex);
+	glDeleteShader(fragment);
+	if(geometryPath != nullptr)
+		glDeleteShader(geometry);
+}
+
+inline void Shader::use()
+{
+	glUseProgram(ID);
+}
+
+inline void Shader::setBool(const std::string & name, bool value) const
+{
+	glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value); 
+}
+
+inline void Shader::setInt(const std::string & name, int value) const
+{
+	glUniform1i(glGetUniformLocation(ID, name.c_str()), value); 
+}
+
+inline void Shader::setFloat(const std::string & name, float value) const
+{
+	glUniform1f(glGetUniformLocation(ID, name.c_str()), value); 
+}
+
+inline void Shader::setVec2(const std::string & name, const glm::vec2 & value) const
+{
+	glUniform2fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]); 
+}
+
+inline void Shader::setVec2(const std::string & name, float x, float y) const
+{
+	glUniform2f(glGetUniformLocation(ID, name.c_str()), x, y); 
+}
+
+inline void Shader::setVec3(const std::string & name, const glm::vec3 & value) const
+{
+	glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]); 
+}
+
+inline void Shader::setVec3(const std::string & name, float x, float y, float z) const
+{
+	glUniform3f(glGetUniformLocation(ID, name.c_str()), x, y, z); 
+}
+
+inline void Shader::setVec4(const std::string & name, const glm::vec4 & value) const
+{
+	glUniform4fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]); 
+}
+
+inline void Shader::setVec4(const std::string & name, float x, float y, float z, float w) const
+{
+	glUniform4f(glGetUniformLocation(ID, name.c_str()), x, y, z, w); 
+}
+
+inline void Shader::setMat2(const std::string & name, const glm::mat2 & mat) const
+{
+	glUniformMatrix2fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+}
+
+inline void Shader::setMat3(const std::string & name, const glm::mat3 & mat) const
+{
+	glUniformMatrix3fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+}
+
+void Shader::setMat4(const std::string &name, const glm::mat4 &mat) const
+{
+	glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+}
+
+inline void Shader::checkCompileErrors(unsigned int shader, std::string type)
+{
+	int success;
+	char infoLog[1024];
+	if (type != "PROGRAM")
+	{
+		glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+		if (!success)
+		{
+			glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+			std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+		}
+	}
+	else
+	{
+		glGetProgramiv(shader, GL_LINK_STATUS, &success);
+		if (!success)
+		{
+			glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+			std::cout << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+		}
+	}
+}
+
+
+#endif // !SHADER_H
---
+++ WjgOGL/OGL_22_GeometryShaderFourHouse/includes/LightShader.fs
@@ -0,0 +1,9 @@
+#version 330 core
+out vec4 FragColor;
+
+void main()
+{
+	//灯光本身的颜色不会被改变
+	FragColor = vec4(1.0f);
+}
+
