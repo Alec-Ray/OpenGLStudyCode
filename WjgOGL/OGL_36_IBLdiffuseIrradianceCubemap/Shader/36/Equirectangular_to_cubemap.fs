73734e0c0a9b02a61278ca7f754c3373d59a2215
WJGwjg <1056929926@qq.com>
2020-05-04 17:53:11 +0800

    IBL Diffuse Irradiance Cubemap

---
+++ WjgOGL/OGL_36_IBLdiffuseIrradianceCubemap/Shader/36/Equirectangular_to_cubemap.fs
@@ -0,0 +1,23 @@
+#version 330 core
+out vec4 FragColor;
+in vec3 WorldPos;
+
+uniform sampler2D equirectangularMap;
+
+const vec2 invAtan = vec2(0.1591, 0.3183);
+vec2 SampleSphericalMap(vec3 v)
+{
+    vec2 uv = vec2(atan(v.z, v.x), asin(v.y));
+    uv *= invAtan;
+    uv += 0.5;
+    return uv;
+}
+
+//将从等柱状投影图采样得到的片段颜色存到立方体贴图的片段上
+void main()
+{		
+    vec2 uv = SampleSphericalMap(normalize(WorldPos));
+    vec3 color = texture(equirectangularMap, uv).rgb;
+    
+    FragColor = vec4(color, 1.0);
+}
